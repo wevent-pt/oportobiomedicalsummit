bb246fcc0fa6d83422259b964567eab45bc206ff
wevent-pt <wevent.pt@gmail.com>
2023-03-14 23:49:33 +0000

    unoptimize images

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
   images: {
+    unoptimized: true,
     domains: [
       'www.notion.so',
       'notion.so',
